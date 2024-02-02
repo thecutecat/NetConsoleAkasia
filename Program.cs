2b763fcdde4870ac62c534237d16807d45ed8a09
Ahmad Pujianto <aisalzaya@outlook.com>
2024-02-03 02:42:05 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//1. Algoritma hitung kata siapa dari string usomaanapaiumasydnip
+//CountWordPossibility("usomaanapaiumasydnip","siapa");
+
+static int CountWordPossibility(string inputString, string targetWord)
+
+{
+    int RealPossibility = 0;
+    Dictionary<char, int> possibility = new Dictionary<char, int>();
+
+    Dictionary<char, int> inputCharCount = new Dictionary<char, int>();
+    Dictionary<char, int> targetCharCount = new Dictionary<char, int>();
+
+    // Count characters in the input string
+    foreach (char c in inputString)
+    {
+        if (inputCharCount.ContainsKey(c))
+            inputCharCount[c]++;
+        else
+            inputCharCount[c] = 1;
+    }
+
+    // Count characters in the target word
+    foreach (char c in targetWord)
+    {
+        if (targetCharCount.ContainsKey(c))
+            targetCharCount[c]++;
+        else
+            targetCharCount[c] = 1;
+    }
+
+    // Calculate the possibility of forming the target word
+    
+    int x = 0;
+    foreach (var kvp in targetCharCount)
+    {
+        char charInTarget = kvp.Key;
+        int countInTarget = kvp.Value;
+
+        if (!inputCharCount.ContainsKey(charInTarget) || inputCharCount[charInTarget] < countInTarget)
+        {
+            // The character in the target word is not present or occurs more times in the input string
+            //possibility is 0;
+            break;
+        }
+        else
+        {
+            // Update the possibility based on the ratio of character occurrences
+            possibility[charInTarget] = (int)inputCharCount[charInTarget] / countInTarget;
+
+            //initiate
+            if (x == 0 && possibility[charInTarget] > 0)
+            {
+                RealPossibility = possibility[charInTarget];
+            }
+
+            if (possibility[charInTarget] == 0)
+            {
+                RealPossibility = 0;
+            }else if(possibility[charInTarget] < RealPossibility)
+            {
+                RealPossibility = possibility[charInTarget];
+            }
+            //get lowest possibility
+
+        }
+        x++;
+    }
+
+    Console.WriteLine($"Kata {targetWord} bisa dibuat {RealPossibility} kali dari {inputString}");
+    return RealPossibility;
+}
+
+//3. perkalian
+
+//PerkalianSederhana(10, 5);
+static int PerkalianSederhana(int j, int k)
+{
+    int hasil = 0;
+    int x = j;
+    while (x > 0)
+    {
+        hasil +=k;
+        x--;
+    }
+
+    Console.WriteLine($"Kata {j} x {k} = {hasil}");
+    return hasil;
+}
+
+//4. count store revenue
+CountRevenue("KawanLamaJewel"); //result  3.250.000
+static double CountRevenue(string StoreName)
+{
+    double revenue = 0.0;
+    Dictionary<string, int> Items = new()
+    {
+        { "Ruby", 5 },
+        { "Topaz", 3 },
+        { "Permata", 1 }
+    };
+
+    Dictionary<string, int> ItemsPrice = new()
+    {
+        { "Ruby", 1000000 },
+        { "Topaz", 1250000 },
+        { "Permata", 3000000 }
+    };
+
+    foreach (KeyValuePair<string, int> item in Items)
+    {
+        revenue += ((int)(item.Value / 2) * ItemsPrice[item.Key]);
+    }
+
+    Console.WriteLine($"revenue maximum {StoreName} = {revenue}");
+    return revenue;
+}
---
+++ query.sql
@@ -0,0 +1,17 @@
+﻿--2.a Get All Student who attend at '2020-06-15','2020-06-17'
+select a.*, s.[Name]  from [dbo].[Absensi] a
+LEFT JOIN student s ON s.Nim=a.Nim
+WHERE thedate IN('2020-06-15','2020-06-17') 
+
+--2b. get number of school attendance on eac student between '2020-06-15' to '2020-06-17' period
+SELECT *, (SELECT count(thedate) FROM Absensi a WHERE a.Nim=s.Nim 
+and thedate BETWEEN '2020-06-15' AND '2020-06-17') as NumberOfAttendance 
+from student s
+
+
+--5 Flowchart online action dengan fitur 
+    --a. product listing
+    --b. product auction 
+
+    --q1 : teknologi yg dipakai : .Net Core dan SQL Server
+    --
